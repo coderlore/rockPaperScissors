5057c5d9d982710585a783110c94b15d58ef437d
coderlore <64398168+coderlore@users.noreply.github.com>
2021-03-21 13:53:12 -1000

    Updated so there is winner declared after 5 rounds.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,6 +1,10 @@
 //Create a random number between 1 - 3
 const getRandomNumber = (min,max) => Math.floor(Math.random() * (max-min + 1)) + min;
 
+// Keeps track of the score
+let playerScore = 0;
+let computerScore = 0;
+
 //Function for single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
     //Assign the computer player a random value
@@ -30,25 +34,44 @@ function playRound(playerSelection, computerSelection) {
         alert("Invalid choice! Restart the game and try again.")
     }
 
+    // Check to see who is the winner and add how many times the player and computer wins/loses
     if (userChoice == result) {
         alert(`Draw! The computer chose ${result}.`)
     }   else if (userChoice == "rock" && result == "scissors") {
         alert(`You win! The computer chose ${result}.`);
+        return ++playerScore;
     }   else if (result == "rock" && userChoice == "scissors") {
         alert(`You lose! The computer chose ${result}.`);
+        return ++computerScore;
     }   else if (userChoice == "rock" && result == "paper") {
         alert(`You lose! The computer chose ${result}.`);
+        return ++computerScore;
     }   else if (result == "rock" && userChoice == "paper") {
         alert(`You win! The computer chose ${result}.`);
+        playerScore = ++playerScore;
     }   else if (result == "scissors" && userChoice == "paper") {
         alert(`You lose! The computer chose ${result}.`);
+        return ++computerScore;
     }   else if (userChoice == "scissors" && result == "paper") {
         alert(`You win! The computer chose ${result}.`);
+        playerScore = ++playerScore;
     }   else {
         alert("Something is wrong! Retry the game.")
     }
     }   
-    
+
+// Play 5 rounds
 for (let step = 0; step < 5; step++) {
     playRound();
+}
+
+// Determine the winner after 5 rounds
+if (playerScore == computerScore) {
+    alert(`Tie! Play again to find a winner.`)
+}   else if (playerScore > computerScore) {
+    alert(`You win! You are the winner of 5 rounds.`)
+}   else if (playerScore < computerScore) {
+    alert(`You lose! The computer is the winner of 5 rounds.`)
+}   else {
+    alert("Something is wrong! Retry the game.")
 }

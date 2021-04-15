5bda2a3a6180524897a4a4bcb692c2884008f5e7
coderlore <64398168+coderlore@users.noreply.github.com>
2021-04-14 20:33:42 -1000

    Redid the entire if/else for the case. Attempted to simplify cases. computerPlay function was updated as an array and is no longer using numbers to identify rock, paper, or scissors.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,89 +5,126 @@ const getRandomNumber = (min,max) => Math.floor(Math.random() * (max-min + 1)) +
 let playerScore = 0;
 let computerScore = 0;
 
+// Prompt user to enter their choice
+const buttons = document.querySelectorAll('button');
+
+function computerPlay() {
+    const choices = ['rock','paper','scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
 //Function for single round of Rock Paper Scissors
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+    if (playerSelection == computerSelection) {
+        alert('Draw');
+    }   else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore += 1;
+    }   else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerScore += 1; 
+    }   else {
+        console.log('Something wrong');
+    }
+}
+
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        playRound(button.value);
+    });
+});
     //Assign the computer player a random value
-    let getComputerChoice = () => getRandomNumber(1,3);
-    let computerPlay = getComputerChoice();
+    /*let getComputerChoice = () => getRandomNumber(1,3);
+    let cpuChoice = getComputerChoice();
 
     //Assign numerical values for rock, paper, scissors
-    if (computerPlay == 1) {
-        result = "rock";
-    }   else if (computerPlay == 2) {
-        result = "paper";
-    }   else if (computerPlay == 3) {
-        result = "scissors";
+    if (cpuChoice == 1) {
+        cpu = "rock";
+    }   else if (cpuChoice == 2) {
+        cpu = "paper";
+    }   else if (cpuChoice == 3) {
+        cpu = "scissors";
     }   else {
-        result = "No Good";
-    }
+        cpu = "No Good";
+    }*/
 
     // Prompt user to enter their choice
-    //let userChoice = window.prompt("Rock, Paper, Scissors? (enter your choice)");
-    const buttons = document.querySelectorAll('button');
-
+    //const buttons = document.querySelectorAll('button');
+////////////////////////////////////////////////////////////////////////
     // we use the .forEach method to iterate through each button
-    buttons.forEach((button) => {
+    /*buttons.forEach((button) => {
 
         // and for each one we add a 'click' listener
-        button.addEventListener('click', () => {
+        button.addEventListener('click', (clear) => {
+            // Function to clear click event
+            function clear() {
+            console.log('Done');
+            button.removeEventListener('click', clear)
+        }
             //alert(button.id);
             if (button.id == 1) {
                 userChoice = 'rock';
-                if (userChoice == result) {
+                if (userChoice == cpu) {
                     alert('Draw'); 
-                    console.log(result);
-                }   else if (userChoice == 'rock' && result == 'scissors') {
+                    console.log(cpu);
+                }   else if (userChoice == 'rock' && cpu == 'scissors') {
                     playerScore += 1; 
+                    if (playerScore == 5) {
+                        console.log('Player is the overall winner');
+                    }
                     console.log('Winner');
-                }   else if (userChoice == 'rock' && result == 'paper') {
+                }   else if (userChoice == 'rock' && cpu == 'paper') {
                     computerScore += 1;
                     console.log('Loser');
+                    if (computerScore == 5) {
+                        console.log('Computer is the overall winner');
+                    }
                 }   else {
                     console.log('Something wrong');
                 }
             } else if (button.id == 2) {
-                console.log('Paper');
+                userChoice = 'paper';
             } else if (button.id == 3) {
-                console.log('scissors');
+                userChoice = 'scissors';
             } else {
                 alert('Invalid choice! Restart the game and try again.');
             }
         });
-    });
-
+    });*/
+    
     // Check to see who is the winner and add how many times the player and computer wins/loses
-    /*if (userChoice == result) {
-        alert(`Draw! The computer chose ${result}.`)
-    }   else if (userChoice == "rock" && result == "scissors") {
-        alert(`You win! The computer chose ${result}.`);
+    /* if (userChoice == cpu) {
+        alert(`Draw! The computer chose ${cpu}.`)
+    }   else if (userChoice == "rock" && cpu == "scissors") {
+        alert(`You win! The computer chose ${cpu}.`);
         return ++playerScore;
-    }   else if (result == "rock" && userChoice == "scissors") {
-        alert(`You lose! The computer chose ${result}.`);
+    }   else if (cpu == "rock" && userChoice == "scissors") {
+        alert(`You lose! The computer chose ${cpu}.`);
         return ++computerScore;
-    }   else if (userChoice == "rock" && result == "paper") {
-        alert(`You lose! The computer chose ${result}.`);
+    }   else if (userChoice == "rock" && cpu == "paper") {
+        alert(`You lose! The computer chose ${cpu}.`);
         return ++computerScore;
-    }   else if (result == "rock" && userChoice == "paper") {
-        alert(`You win! The computer chose ${result}.`);
+    }   else if (cpu == "rock" && userChoice == "paper") {
+        alert(`You win! The computer chose ${cpu}.`);
         playerScore = ++playerScore;
-    }   else if (result == "scissors" && userChoice == "paper") {
-        alert(`You lose! The computer chose ${result}.`);
+    }   else if (cpu == "scissors" && userChoice == "paper") {
+        alert(`You lose! The computer chose ${cpu}.`);
         return ++computerScore;
-    }   else if (userChoice == "scissors" && result == "paper") {
-        alert(`You win! The computer chose ${result}.`);
+    }   else if (userChoice == "scissors" && cpu == "paper") {
+        alert(`You win! The computer chose ${cpu}.`);
         playerScore = ++playerScore;
     }   else {
         alert("Something is wrong! Retry the game.")
     }*/
-}
 
-    playRound();
+
+//    playRound();
 // Play 5 rounds
-/*for (let step = 0; step < 5; step++) {
+/*for (let step = 0; step < 1; step++) {
+    computerPlay();
     playRound();
-}
-
+}*/
+/*
 // Determine the winner after 5 rounds
 if (playerScore == computerScore) {
     alert(`Tie! Play again to find a winner.`)
